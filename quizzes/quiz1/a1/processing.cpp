50f712a0305594dcba237a29a597f3ecf48f8a18
nelsonaxell <axell.hendargo@gmail.com>
2023-03-07 23:00:12 +1100

    a1

--- quizzes/quiz1/a1/processing.cpp
+++ quizzes/quiz1/a1/processing.cpp
@@ -4,12 +4,28 @@
 
 //3) TASK Implement function according to specification in header file
 std::vector<Person> oldestPerson(std::vector<Person> crowd){
-
+std::vector<Person> oldest;
+    unsigned int maxAge = 0;
+    for (auto& person : crowd) {
+        if (person.getAge() > maxAge) {
+            maxAge = person.getAge();
+            oldest.clear();
+        }
+        if (person.getAge() == maxAge) {
+        oldest.push_back(person);
+        }
+    }
+return oldest;
 }
 
 
 //4) TASK Implement function according to specification in header file
 std::vector<Person> eligibleForVaccine(std::vector<Person> crowd, unsigned int ageCutoff){
-
-
+std::vector<Person> eligible;
+for (auto& person : crowd) {
+    if (person.getAge() >= ageCutoff && !person.getVaccinatedStatus() ) {
+        eligible.push_back(person);
+    }
+}
+return eligible;
 }
